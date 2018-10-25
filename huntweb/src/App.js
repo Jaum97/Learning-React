ef59e7d570675f47ef1605df63d3e4b74e91e00f
ChilenoSika <jaum97@yahoo.com.br>
2018-10-24 23:39:14 -0300

    Learning React
    Doing Rocketseat tutorial

---
+++ huntweb/src/App.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles.css';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+        <Header />
+      </div>
+);
+
+
+export default App;
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">Eva 001</header>
+);
+
+export default Header; 
