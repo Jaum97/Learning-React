cd8cc2d0980787dcb5ca293348c85ace7f066479
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-07-09 23:36:39 -0300

    create private.js

---
+++ huntweb/src/components/Header/private.js
@@ -0,0 +1,16 @@
+//https://medium.com/@martin_hotell/10-typescript-pro-tips-patterns-with-or-without-react-5799488d6680
+
+import React, { Component } from 'react'
+
+const _handleChange = Symbol('handleChange')
+
+class App extends Component {
+  [_handleChange] = (ev: import('react').ChangeEvent) => {}
+  render() {
+    return (
+      <div>
+        <input onChange={this[_handleChange]} />
+      </div>
+    )
+  }
+}
