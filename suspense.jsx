bc4f735eb3e50b28e6b7f8281f849352dfd7dc7c
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-10-26 21:16:12 -0300

    create

---
+++ suspense.jsx
@@ -0,0 +1,6 @@
+const ProfilePage = React.lazy(() => import('./ProfilePage')); // Lazy-loaded
+
+// Show a spinner while the profile is loading
+<Suspense fallback={<Spinner />}>
+  <ProfilePage />
+</Suspense>
